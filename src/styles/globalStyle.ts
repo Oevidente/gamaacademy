48369a3ed5b956d3ed681d9aaff3a18884d10adb
André Luiz Alves <andreluiz1902@gmail.com>
2022-06-21 11:17:44 -0300

    meu primeiro projeto com react js

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+        font-family: normal 18px Arial;
+        padding: 0;
+        margin: 0;
+    }
+`
